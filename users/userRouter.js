487a2331c88768b0a20064a1aefb9df17b7f8c78
BuildTools <arthurpisakhov1@gmail.com>
2019-10-23 17:18:18 -0400

    ADD USER, ADD USER POST, GET POST BY ID

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,20 +1,34 @@
 const express = require('express');
-const db = require('./userDb');
+const userDB = require('./userDb');
+const postDB = require('../posts/postDb');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
+  const { name } = req.body;
 
+  if (!name) res.status(400).json({ error: "Please provide name for the user." })
+  else 
+    userDB.insert(req.body)
+      .then(user => res.status(201).json(user))
+      .catch(() => res.status(500).json({ error: "There was an error while saving the user to the database" }))
 });
 
 router.post('/:id/posts', (req, res) => {
-
+  const { user_id, text } = req.body;
+  console.log(req.body)
+  if (!text) res.status(400).json({ error: "Please provide text for the post." })
+  else if (!user_id) res.status(404).json({ message: "The user with the specified ID does not exist." })
+  else 
+    postDB.insert(req.body)
+      .then(post => res.status(201).json(post))
+      .catch(() => res.status(500).json({ error: "There was an error while saving the post to the database" }))
 });
 
 router.get('/', (req, res) => {
   const sortField = req.query.sortBy || 'id';
-  console.log('working?')
-  db.get()
+
+  userDB.get()
     .then(users => {
       const response = users.sort((a,b) => a[sortField] < b[sortField] ? -1 : 1)
       res.status(200).json(response)
@@ -23,7 +37,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  db.getById(req.params.id)
+  userDB.getById(req.params.id)
     .then(user => {
       if (user) res.status(200).json(user)
       else res.status(404).json({ message: "The user with the specified ID does not exist." })
@@ -32,7 +46,12 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/posts', (req, res) => {
-
+  userDB.getUserPosts(req.params.id)
+    .then(posts => {
+      if (posts.length > 0) res.status(200).json(posts)
+      else res.status(404).json({ message: "The posts with the specified user ID does not exist." })
+    })
+    .catch(() => res.status(500).json({ error: "The posts information could not be retrieved." }))
 });
 
 router.delete('/:id', (req, res) => {
