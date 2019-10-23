761b3c7bf73a379c31372d9826abdb651c6bfb67
BuildTools <arthurpisakhov1@gmail.com>
2019-10-23 17:03:12 -0400

    GET by ID

--- users/userRouter.js
+++ users/userRouter.js
@@ -13,7 +13,7 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   const sortField = req.query.sortBy || 'id';
-
+  console.log('working?')
   db.get()
     .then(users => {
       const response = users.sort((a,b) => a[sortField] < b[sortField] ? -1 : 1)
@@ -23,7 +23,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-
+  db.getById(req.params.id)
+    .then(user => {
+      if (user) res.status(200).json(user)
+      else res.status(404).json({ message: "The user with the specified ID does not exist." })
+    })
+    .catch(() => res.status(500).json({ error: "The users information could not be retrieved." }))
 });
 
 router.get('/:id/posts', (req, res) => {
