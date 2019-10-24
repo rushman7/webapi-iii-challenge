341672e471098228f181a6ffe799bb45d1f400d3
BuildTools <arthurpisakhov1@gmail.com>
2019-10-23 20:39:00 -0400

    prepping for deployment

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
+require('dotenv').config();
 
 const server = express();
+const port = process.env.PORT;
 
 server.use(express.json())
 server.use(cors());
@@ -25,7 +27,11 @@ server.use((err, req, rest, next) => {
   res.status(500).json({ message: 'There was an error performing the required operation', error: err })
 })
 
+server.get('/', (req, res) => {
+  res.send(`<h2>App is Deployed</h2>`)
+})
+
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter)
 
-server.listen(5000, () => console.log('Server is running on port 5000'));
+server.listen(port, () => console.log(`Server is running on port ${port}`));
